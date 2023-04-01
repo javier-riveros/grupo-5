8451bdf30ddaa6c55a1f32fab55ce16cfa2567ce
javier-riveros <128341390+javier-riveros@users.noreply.github.com>
2023-03-31 21:14:36 -0300

    molestaba un poco
    creo que solo era para probar si funcionaba pero ya lo saque por que anda bien

--- index.js
+++ index.js
@@ -9,11 +9,3 @@ const closeMenu = () => {
   menuButton.style.right = '-70%';
 }
 
-
-window.addEventListener('resize', () => {
-  if(window.innerWidth >= 576) {
-    openMenu();
-  }else {
-    closeMenu();
-  }
-});
