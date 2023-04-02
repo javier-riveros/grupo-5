80ddcc376a1bedd7c7baa761d4e8fff11d2f3954
jpgalarza <jpgalarza@hotmail.com.ar>
2023-04-01 21:06:03 -0300

    Corrección de menú responsivo

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const openMenu = () => {
 
 const closeMenu = () => {
   const menuButton = document.querySelector('#menu-container');
-  menuButton.style.right = '-70%';
+  menuButton.style.right = '-71%';
 }
 
 
