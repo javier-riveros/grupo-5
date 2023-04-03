bbbabbddc78be9795bf1ec99ecec6e85964165f6
coli <tecnicoli@gmail.com>
2023-04-02 21:23:18 -0300

    Laboratio de API funcionando
    Pude hacer funcionar la API.
    
    Subo 2 archivos getAnimales a modo ejemplo que consulta reiterada veces la API para mostrar la info en el HTML.

--- getAnimals.js
+++ getAnimals.js
@@ -1,31 +1,70 @@
 
-const listarAnimales = () => {
+const animales= ["cheetah", "Bengal Tiger", "Sea Turtle", "Polar Bear", "Giant Panda Bear", "cheetah"]            // LISTDO DE ANIMALES A MOSTRAR
 
-    let animal = document.getElementById("idAnimal").value;    // ME TRAIGO EL NOMBRE DEL ANIMAL
-    console.log(animal);
-    let infoAnimal;
 
-    $.ajax({
-        method: 'GET',
-        url: 'https://api.api-ninjas.com/v1/animals?name=' + animal,
+var animalInfoID = setInterval(iterarAnimalInfo, 5000);
+let x=0;
+
+function iterarAnimalInfo(a) {
+
+
+    console.log("animal " + x + " de " + animales.length);      // MUESTRO EN CONSOLA CUANTOS ANIMALES MOSTRE DE LA LISTA
+
+    if (x < animales.length){
+     
+        showAnimalInfo(x);
+        x++;
+
+
+    }else{
+
+        clearInterval (animalInfoID);
+    }
+    
+}
+
+
+
+const showAnimalInfo = (i) => {
+
+    let animalLocations;
+    let animalName;
+    let animalCharacteristics;
+
+
+    $.ajax({                                                              // CONSULTO LA API
+        method: 'GET',            
+        url: 'https://api.api-ninjas.com/v1/animals?name=' + animales[i],
         headers: { 'X-Api-Key': 'Kukygbvlj/Hfvv/HoF4ieQ==7bSQFvdCMcN8qvDz'},
         contentType: 'application/json',
         success: function(result) {
-            console.log(result);
-            
-//            infoAnimal = JSON.parse(result);                    //  INTENTO DE PARSEAR EL JSON
+           
+
+            animalName=result[0].name;
+            animalLocations=result[0].locations;
+            animalCharacteristics=result[0].characteristics;
+
 
         }, error: function ajaxError(jqXHR) {
             console.error('Error: ', jqXHR.responseText);
         }
     })
 
+setTimeout (() => {                         // espero 2 segundos a tener la respuesta de la API
+ 
+ //   console.log (animalName + animalLocations + animalCharacteristics.age_of_weaning);
 
+document.getElementById("info").innerHTML = 
+"Animal: " + animalName + "<br>" + 
+"Locacion: " + animalLocations + "<br>" + 
+"Presa: " + animalCharacteristics.prey + "<br>" +
+"Comportamiento de grupo: " + animalCharacteristics.group_behavior + "<br>" +
+"Ejemplares estimados: " + animalCharacteristics.estimated_population_size + "<br>" +
+"Periodo de gestacion: " + animalCharacteristics.gestation_period + "<br>" +    // ESCRIBO EL VALOR EN EL <p>
+"Esperanza de vida: " + animalCharacteristics.lifespan + "<br>";
 
-  
-  document.getElementById("info").innerHTML = "hola che";          // ESCRIBO EL VALOR EN EL <p>
-//  document.getElementById("info").innerHTML = infoAnimal.name;    // ESCRIBO EL VALOR EN EL <p>
-
+}, 2000);
+ 
 
 };
 
