f4d473f4635c77d6a4d3ad8348281ee7ae7d0330
Moises <zmoises@gmail.com>
2023-04-17 18:07:03 -0300

    cambios carousel
    agregando url footer

--- js/carousel.js
+++ js/carousel.js
@@ -1,7 +1,7 @@
 
 window.addEventListener('load', function(){
     new Glider(document.querySelector('.carousel__lista'), {
-    slidesToShow: 1,
+    slidesToShow: 2,
     slidesToScroll: 1,
     dots: '.carousel__indicadores',
     draggable: true,
@@ -15,14 +15,14 @@ window.addEventListener('load', function(){
         breakpoint: 576,
         settings: {
           // se ajusta a dos fotos por pantalla
-          slidesToShow:2,
+          slidesToShow:3,
           slidesToScroll: 2,
                 }
       },{
         // pantallas mayores que >= 1024px
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 4,
           slidesToScroll: 3,
          
         }
