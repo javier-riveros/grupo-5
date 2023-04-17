2030b0f85ab20f77b8be04e8f9b1b801d207dca3
Moises <zmoises@gmail.com>
2023-04-17 17:47:54 -0300

    cambios
    cambios

--- carousel.js
+++ carousel.js
@@ -1,13 +1,32 @@
 
 window.addEventListener('load', function(){
     new Glider(document.querySelector('.carousel__lista'), {
-    slidesToShow: 4,
+    slidesToShow: 1,
     slidesToScroll: 1,
     dots: '.carousel__indicadores',
     draggable: true,
     arrows: {
       prev: '.glider-prev',
       next: '.glider-next'
-    }
+    },
+    responsive: [
+      {
+        // pantallas mayores que >= 775px
+        breakpoint: 576,
+        settings: {
+          // se ajusta a dos fotos por pantalla
+          slidesToShow:2,
+          slidesToScroll: 2,
+                }
+      },{
+        // pantallas mayores que >= 1024px
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+         
+        }
+      }
+    ]
   });
   });
